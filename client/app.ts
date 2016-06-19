2c5fb7b2325c2195a99fad9a6536073dac8c105b
Chicloon <Chicloon@gmail.com>
2016-06-19 23:16:25 +0900

    db is workin. do NOT update

--- client/app.ts
+++ client/app.ts
@@ -1,11 +1,19 @@
 import 'reflect-metadata';
 import { Component } from '@angular/core';
-import { bootstrap } from '@angular/platform-browser-dynamic';
- 
+import { bootstrap } from 'angular2-meteor-auto-bootstrap';
+import { Parties }   from '../collections/parties';
+import { Mongo }     from 'meteor/mongo';
+
 @Component({
   selector: 'app',
   templateUrl: '/client/app.html'
 })
-class Socially { }
- 
+class Socially {
+  parties: Mongo.Cursor<Object>;
+
+  constructor () {
+    this.parties = Parties.find();
+  }
+}
+
 bootstrap(Socially);
